e8414ea5a01052d3405d65a74563cb580194c945
Baozhu Zuo <zuobaozhu@gmail.com>
2020-02-02 12:51:13 +0800

    Pretty printed the Arduino code with astyle

--- src/epd2in7b.h
+++ src/epd2in7b.h
@@ -1,28 +1,28 @@
 /**
- *  @filename   :   epd2in7b.h
- *  @brief      :   Header file for Dual-color e-paper library epd2in7b.cpp
- *  @author     :   Yehui from Waveshare
- *  
- *  Copyright (C) Waveshare     August 10 2017
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documnetation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to  whom the Software is
- * furished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS OR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
+    @filename   :   epd2in7b.h
+    @brief      :   Header file for Dual-color e-paper library epd2in7b.cpp
+    @author     :   Yehui from Waveshare
+
+    Copyright (C) Waveshare     August 10 2017
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documnetation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to  whom the Software is
+    furished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in
+    all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS OR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+    THE SOFTWARE.
+*/
 
 #ifndef EPD2IN7B_H
 #define EPD2IN7B_H
@@ -46,10 +46,10 @@
 #define DATA_STOP                                   0x11
 #define DISPLAY_REFRESH                             0x12
 #define DATA_START_TRANSMISSION_2                   0x13
-#define PARTIAL_DATA_START_TRANSMISSION_1           0x14 
-#define PARTIAL_DATA_START_TRANSMISSION_2           0x15 
+#define PARTIAL_DATA_START_TRANSMISSION_1           0x14
+#define PARTIAL_DATA_START_TRANSMISSION_2           0x15
 #define PARTIAL_DISPLAY_REFRESH                     0x16
-#define LUT_FOR_VCOM                                0x20 
+#define LUT_FOR_VCOM                                0x20
 #define LUT_WHITE_TO_WHITE                          0x21
 #define LUT_BLACK_TO_WHITE                          0x22
 #define LUT_WHITE_TO_BLACK                          0x23
@@ -79,7 +79,7 @@ extern const unsigned char lut_bb[];
 extern const unsigned char lut_wb[];
 
 class Epd : EpdIf {
-public:
+  public:
     unsigned int width;
     unsigned int height;
 
@@ -100,7 +100,7 @@ public:
     void ClearFrame(void);
     void Sleep(void);
 
-private:
+  private:
     unsigned int reset_pin;
     unsigned int dc_pin;
     unsigned int cs_pin;
--- src/epdpaint.cpp
+++ src/epdpaint.cpp
@@ -1,28 +1,28 @@
 /**
- *  @filename   :   epdpaint.cpp
- *  @brief      :   Paint tools
- *  @author     :   Yehui from Waveshare
- *  
- *  Copyright (C) Waveshare     September 9 2017
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documnetation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to  whom the Software is
- * furished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS OR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
+    @filename   :   epdpaint.cpp
+    @brief      :   Paint tools
+    @author     :   Yehui from Waveshare
+
+    Copyright (C) Waveshare     September 9 2017
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documnetation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to  whom the Software is
+    furished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in
+    all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS OR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+    THE SOFTWARE.
+*/
 
 #include <avr/pgmspace.h>
 #include "epdpaint.h"
@@ -39,8 +39,8 @@ Paint::~Paint() {
 }
 
 /**
- *  @brief: clear the image
- */
+    @brief: clear the image
+*/
 void Paint::Clear(int colored) {
     for (int x = 0; x < this->width; x++) {
         for (int y = 0; y < this->height; y++) {
@@ -50,9 +50,9 @@ void Paint::Clear(int colored) {
 }
 
 /**
- *  @brief: this draws a pixel by absolute coordinates.
- *          this function won't be affected by the rotate parameter.
- */
+    @brief: this draws a pixel by absolute coordinates.
+            this function won't be affected by the rotate parameter.
+*/
 void Paint::DrawAbsolutePixel(int x, int y, int colored) {
     if (x < 0 || x >= this->width || y < 0 || y >= this->height) {
         return;
@@ -73,8 +73,8 @@ void Paint::DrawAbsolutePixel(int x, int y, int colored) {
 }
 
 /**
- *  @brief: Getters and Setters
- */
+    @brief: Getters and Setters
+*/
 unsigned char* Paint::GetImage(void) {
     return this->image;
 }
@@ -99,38 +99,38 @@ int Paint::GetRotate(void) {
     return this->rotate;
 }
 
-void Paint::SetRotate(int rotate){
+void Paint::SetRotate(int rotate) {
     this->rotate = rotate;
 }
 
 /**
- *  @brief: this draws a pixel by the coordinates
- */
+    @brief: this draws a pixel by the coordinates
+*/
 void Paint::DrawPixel(int x, int y, int colored) {
     int point_temp;
     if (this->rotate == ROTATE_0) {
-        if(x < 0 || x >= this->width || y < 0 || y >= this->height) {
+        if (x < 0 || x >= this->width || y < 0 || y >= this->height) {
             return;
         }
         DrawAbsolutePixel(x, y, colored);
     } else if (this->rotate == ROTATE_90) {
-        if(x < 0 || x >= this->height || y < 0 || y >= this->width) {
-          return;
+        if (x < 0 || x >= this->height || y < 0 || y >= this->width) {
+            return;
         }
         point_temp = x;
         x = this->width - y;
         y = point_temp;
         DrawAbsolutePixel(x, y, colored);
     } else if (this->rotate == ROTATE_180) {
-        if(x < 0 || x >= this->width || y < 0 || y >= this->height) {
-          return;
+        if (x < 0 || x >= this->width || y < 0 || y >= this->height) {
+            return;
         }
         x = this->width - x;
         y = this->height - y;
         DrawAbsolutePixel(x, y, colored);
     } else if (this->rotate == ROTATE_270) {
-        if(x < 0 || x >= this->height || y < 0 || y >= this->width) {
-          return;
+        if (x < 0 || x >= this->height || y < 0 || y >= this->width) {
+            return;
         }
         point_temp = x;
         x = y;
@@ -140,8 +140,8 @@ void Paint::DrawPixel(int x, int y, int colored) {
 }
 
 /**
- *  @brief: this draws a charactor on the frame buffer but not refresh
- */
+    @brief: this draws a charactor on the frame buffer but not refresh
+*/
 void Paint::DrawCharAt(int x, int y, char ascii_char, sFONT* font, int colored) {
     int i, j;
     unsigned int char_offset = (ascii_char - ' ') * font->Height * (font->Width / 8 + (font->Width % 8 ? 1 : 0));
@@ -163,13 +163,13 @@ void Paint::DrawCharAt(int x, int y, char ascii_char, sFONT* font, int colored) 
 }
 
 /**
-*  @brief: this displays a string on the frame buffer but not refresh
+    @brief: this displays a string on the frame buffer but not refresh
 */
 void Paint::DrawStringAt(int x, int y, const char* text, sFONT* font, int colored) {
     const char* p_text = text;
     unsigned int counter = 0;
     int refcolumn = x;
-    
+
     /* Send the string character by character on EPD */
     while (*p_text != 0) {
         /* Display one character on EPD */
@@ -183,7 +183,7 @@ void Paint::DrawStringAt(int x, int y, const char* text, sFONT* font, int colore
 }
 
 /**
-*  @brief: this draws a line on the frame buffer
+    @brief: this draws a line on the frame buffer
 */
 void Paint::DrawLine(int x0, int y0, int x1, int y1, int colored) {
     /* Bresenham algorithm */
@@ -193,21 +193,21 @@ void Paint::DrawLine(int x0, int y0, int x1, int y1, int colored) {
     int sy = y0 < y1 ? 1 : -1;
     int err = dx + dy;
 
-    while((x0 != x1) && (y0 != y1)) {
-        DrawPixel(x0, y0 , colored);
-        if (2 * err >= dy) {     
+    while ((x0 != x1) && (y0 != y1)) {
+        DrawPixel(x0, y0, colored);
+        if (2 * err >= dy) {
             err += dy;
             x0 += sx;
         }
         if (2 * err <= dx) {
-            err += dx; 
+            err += dx;
             y0 += sy;
         }
     }
 }
 
 /**
-*  @brief: this draws a horizontal line on the frame buffer
+    @brief: this draws a horizontal line on the frame buffer
 */
 void Paint::DrawHorizontalLine(int x, int y, int line_width, int colored) {
     int i;
@@ -217,7 +217,7 @@ void Paint::DrawHorizontalLine(int x, int y, int line_width, int colored) {
 }
 
 /**
-*  @brief: this draws a vertical line on the frame buffer
+    @brief: this draws a vertical line on the frame buffer
 */
 void Paint::DrawVerticalLine(int x, int y, int line_height, int colored) {
     int i;
@@ -227,7 +227,7 @@ void Paint::DrawVerticalLine(int x, int y, int line_height, int colored) {
 }
 
 /**
-*  @brief: this draws a rectangle
+    @brief: this draws a rectangle
 */
 void Paint::DrawRectangle(int x0, int y0, int x1, int y1, int colored) {
     int min_x, min_y, max_x, max_y;
@@ -235,7 +235,7 @@ void Paint::DrawRectangle(int x0, int y0, int x1, int y1, int colored) {
     max_x = x1 > x0 ? x1 : x0;
     min_y = y1 > y0 ? y0 : y1;
     max_y = y1 > y0 ? y1 : y0;
-    
+
     DrawHorizontalLine(min_x, min_y, max_x - min_x + 1, colored);
     DrawHorizontalLine(min_x, max_y, max_x - min_x + 1, colored);
     DrawVerticalLine(min_x, min_y, max_y - min_y + 1, colored);
@@ -243,7 +243,7 @@ void Paint::DrawRectangle(int x0, int y0, int x1, int y1, int colored) {
 }
 
 /**
-*  @brief: this draws a filled rectangle
+    @brief: this draws a filled rectangle
 */
 void Paint::DrawFilledRectangle(int x0, int y0, int x1, int y1, int colored) {
     int min_x, min_y, max_x, max_y;
@@ -252,14 +252,14 @@ void Paint::DrawFilledRectangle(int x0, int y0, int x1, int y1, int colored) {
     max_x = x1 > x0 ? x1 : x0;
     min_y = y1 > y0 ? y0 : y1;
     max_y = y1 > y0 ? y1 : y0;
-    
+
     for (i = min_x; i <= max_x; i++) {
-      DrawVerticalLine(i, min_y, max_y - min_y + 1, colored);
+        DrawVerticalLine(i, min_y, max_y - min_y + 1, colored);
     }
 }
 
 /**
-*  @brief: this draws a circle
+    @brief: this draws a circle
 */
 void Paint::DrawCircle(int x, int y, int radius, int colored) {
     /* Bresenham algorithm */
@@ -276,8 +276,8 @@ void Paint::DrawCircle(int x, int y, int radius, int colored) {
         e2 = err;
         if (e2 <= y_pos) {
             err += ++y_pos * 2 + 1;
-            if(-x_pos == y_pos && e2 <= x_pos) {
-              e2 = 0;
+            if (-x_pos == y_pos && e2 <= x_pos) {
+                e2 = 0;
             }
         }
         if (e2 > x_pos) {
@@ -287,7 +287,7 @@ void Paint::DrawCircle(int x, int y, int radius, int colored) {
 }
 
 /**
-*  @brief: this draws a filled circle
+    @brief: this draws a filled circle
 */
 void Paint::DrawFilledCircle(int x, int y, int radius, int colored) {
     /* Bresenham algorithm */
@@ -306,14 +306,14 @@ void Paint::DrawFilledCircle(int x, int y, int radius, int colored) {
         e2 = err;
         if (e2 <= y_pos) {
             err += ++y_pos * 2 + 1;
-            if(-x_pos == y_pos && e2 <= x_pos) {
+            if (-x_pos == y_pos && e2 <= x_pos) {
                 e2 = 0;
             }
         }
-        if(e2 > x_pos) {
+        if (e2 > x_pos) {
             err += ++x_pos * 2 + 1;
         }
-    } while(x_pos <= 0);
+    } while (x_pos <= 0);
 }
 
 /* END OF FILE */
